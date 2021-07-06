18777ee506f23f7ab45477d1ab0b492f856631fe
MorardRamiro <rramiromorard@gmail.com>
2021-07-06 19:19:55 -0300

    6/7

--- api/src/controllers/activities.js
+++ api/src/controllers/activities.js
@@ -1,17 +1,29 @@
 const { Activity } = require("../db");
+const { Country } = require("../db");
+const { Op } = require("sequelize");
 
 const createActivity = async (req, res, next) => {
     try {
-        const { id, name, difficulty, duration, season } = req.body;
-        if (!id || !name || !difficulty || !duration || !season) return res.send("NO");
+        const { id, name, difficulty, duration, season, countries } = req.body;
+        /* if (!id || !name || !difficulty || !duration || !season || !countries) return res.send("NO"); */
         const createdActivity = await Activity.create({ 
             id: id, 
             name: name,
             difficulty: difficulty, 
             duration: duration, 
-            season: season
+            season: season,
+            countries: countries
         });
+        const findCountries = await Country.findAll({
+            where: { id: {
+                [Op.in]: countries
+              }
+            }
+        });
+        await createdActivity.addCountries(findCountries);
         res.json(createdActivity);
+        
+        
     } catch (err) {
         next(err)
     }
--- api/src/controllers/countries.js
+++ api/src/controllers/countries.js
@@ -1,27 +1,37 @@
 const axios = require("axios");
+const { Op } = require("sequelize");
 
 const { Country } = require("../db");
+const { Activity } = require("../db");
 
 const getAllCountries = async (req, res, next) => {
+    
     const { name } = req.query;
     if (name) {
-        const namedCountry = await Country.findOne({
-            where: {
-                name: name
-            }
-        });
-        res.json(namedCountry);
+        try {
+            const namedCountry = await Country.findAll({
+                where: {
+                    name: {
+                        [Op.iLike]: "%" + name + "%"
+                    }
+                }
+            });
+            res.json(namedCountry);
+        } catch (err) {
+            next(err)
+        } 
     }
-
-
-    /* const findCountries = await Country.findAll({
-        limit: 10
+    const findCountries = await Country.findAll({
+        limit: 10,
+        offset: req.query.page,
+        attributes: {
+            exclude: ['id','capital', 'region', 'area', 'population', 'createdAt', 'updatedAt']
+            },
+        /* order: [["name", req.query.order]] */
     });
-    console.log(findCountries);
-    if (findCountries !== []) {
-        console.log(findCountries);
+    if (findCountries.length) {
         res.json(findCountries);
-    } else { } */
+    } else { 
        try {
             const countryApi = await axios.get(`https://restcountries.eu/rest/v2/all`);
             const countriesArray = countryApi.data.map(
@@ -38,12 +48,19 @@ const getAllCountries = async (req, res, next) => {
             );
             await Country.bulkCreate(countriesArray);
             const findCountries = await Country.findAll({
-                limit: 10
+                limit: 10,
+                offset: req.query.page,
+                attributes: {
+                    exclude: ['id','capital', 'region', 'area', 'population', 'createdAt', 'updatedAt']
+                },
+                /* order: [["name", req.query.order]] */
+                /* include: { model: Activity } */
             });
             res.json(findCountries)
         } catch (err) {
             next(err)
         }
+    }
     /* const countryApi = await axios.get(`https://restcountries.eu/rest/v2/all`);
     const countryMine = await Country.findAll();
     Promise.all([countryApi, countryMine])
@@ -59,7 +76,7 @@ const getAllCountries = async (req, res, next) => {
 const getCountryById = async (req, res, next) => {
     try {
         const { id } = req.params;
-        const findCountry = await Country.findByPk(id);
+        const findCountry = await Country.findByPk(id, {include: Activity});
         res.json(findCountry);
     } catch (err) {
         next(err)
